bba1c3361373751542b43a955981bd05866a08b9
albugrimenko <alex.bugrimenko@gmail.com>
2019-01-08 20:23:37 -0500

    charts are ready

--- DB/MsSQL_PerfmonE/DeploymentScripts/04_StoredProcs.sql
+++ DB/MsSQL_PerfmonE/DeploymentScripts/04_StoredProcs.sql
@@ -953,6 +953,85 @@ SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
+CREATE PROCEDURE [dbo].[GetMetricValuesWithStats]
+	@StartDate date, @EndDate date,
+
+	@ServerID int = null, @ServerName nvarchar(200) = null, 
+	@MetricSetID int = null, @MetricSetName nvarchar(200) = null,
+	@MetricID int = null, @MetricName nvarchar(200) = null
+
+/* Gets all recorded values for a particular server/metric for specified date range
+
+exec GetMetricValuesWithStats @StartDate = '12/16/2018', @EndDate = '12/16/2018',
+	@ServerID=4, @MetricSetName='Processor(_Total)', 
+	@MetricName='% Processor Time'
+*/
+--with recompile
+
+AS
+set nocount on;
+--set transaction isolation level snapshot;
+
+---- params 
+if @EndDate is NULL
+	set @EndDate = getdate()
+if @StartDate is NULL
+	set @StartDate = dateadd(week, -1, @EndDate)
+
+if @ServerID is null and len(isnull(@ServerName,'')) > 0
+	exec @ServerID=GetLookupID @ObjectName='servers', @ObjectValue=@ServerName, @IsAutoAdd=0
+
+if @MetricSetID is null and len(isnull(@MetricSetName,'')) > 0
+	exec @MetricSetID=GetLookupID @ObjectName='metricsets', @ObjectValue=@MetricSetName, @IsAutoAdd=0
+
+if @MetricID is null and len(isnull(@MetricName,'')) > 0
+	exec @MetricID=GetLookupID @ObjectName='metrics', @ObjectValue=@MetricName, @IsAutoAdd=0
+
+if (@ServerID is NULL) begin
+	raiserror('ServerID or ServerName is required.', 16, 1)
+	RETURN -1
+end
+if (@MetricSetID is NULL) begin
+	raiserror('MetricSetID or MetricSetName is required.', 16, 1)
+	RETURN -1
+end
+if (@MetricID is NULL) begin
+	raiserror('MetricID or MetricName is required.', 16, 1)
+	RETURN -1
+end
+
+---- get data
+; with fdates as (
+	select ID, TheDate, DayInYear, DayInWeek
+	from Dates (nolock)
+	where TheDate between @StartDate and @EndDate
+)
+select --top(2147483647)
+	[Date] = d.TheDate,
+	[Time] = t.TheTime,
+	Value = mv.Value,
+	StatValue_Lo = s.Value_Lo,
+	StatValue_Hi = s.Value_Hi,
+	StatValue_Avg = s.Value_Avg,
+	StatValue_Std = s.Value_Std
+from MetricValues mv (nolock)
+	join fdates d on mv.DateID = d.ID 
+		and mv.DayInYear = d.DayInYear	-- required for proper partitions usage
+	join Times t (nolock) on mv.TimeID = t.ID
+
+	left join MetricValueStats s (nolock) on mv.ServerID = s.ServerID and mv.MetricSetID = s.MetricSetID and mv.MetricID = s.MetricID
+		and s.GrHours = 1
+		and d.DayInWeek = s.DayInWeek and mv.TimeID between s.StartTimeID and s.EndTimeID
+where --m.DayInYear in (select DayInYear from fdates group by DayInYear)
+	mv.ServerID = @ServerID
+	and mv.MetricSetID = @MetricSetID
+	and mv.MetricID = @MetricID
+order by d.ID, t.ID
+
+RETURN 1
+GO
+GRANT EXECUTE ON [dbo].[GetMetricValuesWithStats] TO [reporter] AS [dbo]
+GO
 CREATE PROCEDURE [dbo].[GetPotentialIssues]
 	@ServerID int = null, @ServerName nvarchar(200) = null, 
 	@MetricSetID int = null, @MetricSetName nvarchar(200) = null,
--- WebUI/Client/js/perfmon_chart.js
+++ WebUI/Client/js/perfmon_chart.js
@@ -1,79 +1,70 @@
-// required: python -m SimpleHTTPServer 8000 &
-// file:///home/alex/Documents/source/PerfMon/WebUI/Client/chart.html
-
 var chart_fmt = {
 	margin: 20,
-	color_bkg: '#e9ecef',
+	color_bkg: '#f4f4f4',
 	color_primeline: '#007e7e',
 	color_danger: '#d43f3a',
 	color_helperline: '#d27c00',
 	is_showZeroLine: false,
+	is_statAsArea: true,	// shows stat line as area, otherwise as dashed line
 	precision: 4,
 	formatFullTime: d3.timeFormat("%a %b %d, %Y @ %H:%M"),
 	formatDate: d3.timeFormat("%a %b %d, %Y"),
 	formatTime: d3.timeFormat("%H:%M")
 };
 
-function get_data_mv(server_id, set_id, metric_id, start_date, end_date, dest_id='', chart_title=''){
-	var chartdata = null;
-	if (dest_id) {
-		$('#' + dest_id).html('<p class="m-t text-center">...working...<br><img src="./img/ajax-loader.gif" alt="...working..." /></p>');
+function getChart(dest_id, chart_title) {
+	if (viewModel) {
+		var server_id = viewModel.selectedServer().id,
+			set_id = viewModel.selectedMetricSet().id,
+			metric_id = viewModel.selectedMetric().id,
+			sd = $('#dateStart').val(),
+			ed = $('#dateEnd').val();
+		drawchart(server_id, set_id, metric_id, sd, ed, dest_id, chart_title);
 	}
-	if (server_id == 0 && set_id == 0 && metric_id == 0) {
-		// test mode - use static file to show chart
-		chart_title += ' (test mode)';
-		$.getJSON("/data/metricvalues.raw.json", function(data){
-				chartdata = data.results;
-				//alert(chartdata.length);
-			})
-			.done(function() {
-				if (dest_id) {
-					$('#' + dest_id).html('');
-					drawchart_mv(dest_id, chartdata, chart_title);
-				}
-			})
-			.fail(function(error){
-				if (dest_id) {
-					$('#' + dest_id).html('<p class="m-t text-danger"><b>ERROR:</b> Metric Values cannot be loaded at this time.</p>');
-				}
-				else {
-					alert('ERROR: Metric Values cannot be loaded at this time.');
-				}
-			})
-			;
+	else {
+		alert('ERROR: Cannot get selected values from a page - viewModel is required (KnockOut observable).');
 	}
-	return chartdata;
+	return true;
 }
 
-function get_data_mvdet(server_id, set_id, metric_id, start_date, end_date, dest_id='', chart_title=''){
-	var chartdata = null;
-	if (dest_id) {
-		$('#' + dest_id).html('<p class="m-t text-center">...working...<br><img src="./img/ajax-loader.gif" alt="...working..." /></p>');
-	}
+function drawchart(server_id, set_id, metric_id, start_date, end_date, dest_id, chart_title=''){
+	$('#' + dest_id).html('<p class="m-t text-center">...working...<br><img src="./img/ajax-loader.gif" alt="...working..." /></p>');
+	var url = "";
+	var is_raw = (start_date == end_date);
 	if (server_id == 0 && set_id == 0 && metric_id == 0) {
-		// test mode - use static file to show chart
+		url = "/data/metricvalues" + (is_raw ? ".raw" : "det") + ".json";
 		chart_title += ' (test mode)';
-		$.getJSON("/data/metricvaluesdet.json", function(data){
-				chartdata = data.results;
-				//alert(chartdata.length);
-			})
-			.done(function() {
-				if (dest_id) {
-					$('#' + dest_id).html('');
-					drawchart_mvdet(dest_id, chartdata, chart_title);
-				}
-			})
-			.fail(function(error){
-				if (dest_id) {
-					$('#' + dest_id).html('<p class="m-t text-danger"><b>ERROR:</b> Metric Values cannot be loaded at this time.</p>');
+	}
+	else {
+		url = baseURL + "/metricvalues";
+		url += (is_raw) ? "/raw/" : "/det/";
+		url += server_id + "/" + set_id + "/" + metric_id;
+		url += "?sd=" + start_date + "&ed=" + end_date;
+	}
+
+	var chartdata = null;
+	$.getJSON(url, function(data){
+			chartdata = data.results;
+			if (chartdata.length == 0) {
+				alert("There is no data available for the selected set of filters.");
+				$('#' + dest_id).html('<p class="m-t text-danger"><b>WARNING:</b> There is no Metric Values available.</p>');
+			}
+		})
+		.done(function() {
+			if (chartdata.length > 0) {
+				$('#' + dest_id).html('');
+				if (is_raw) {
+					drawchart_mv(dest_id, chartdata, chart_title);
 				}
 				else {
-					alert('ERROR: Metric Values cannot be loaded at this time.');
+					drawchart_mvdet(dest_id, chartdata, chart_title);
 				}
-			})
-			;
-	}
-	return chartdata;
+			}
+		})
+		.fail(function(error){
+			$('#' + dest_id).html('<p class="m-t text-danger"><b>ERROR:</b> Metric Values cannot be loaded at this time.</p>');
+		});
+	return true;
 }
 
 function get_datastats_mv(data){
@@ -90,11 +81,13 @@ function get_datastats_mv(data){
 	else {
 		var minValue = d3.min(data, function(el){return +el.value;}),
 			maxValue = d3.max(data, function(el){return +el.value;});
+		var minSValue = d3.min(data, function(el){return +el.statvalue_lo;}),
+			maxSValue = d3.max(data, function(el){return +el.statvalue_hi;});
 		var minDate = d3.min(data, function(el){return new Date(el.date + ' ' + el.time);}),
 			maxDate = d3.max(data, function(el){return new Date(el.date + ' ' + el.time);});
 		return {
-			'min' : minValue,
-			'max' : maxValue,
+			'min' : Math.min(minValue, minSValue),
+			'max' : Math.max(maxValue, maxSValue),
 			'minDate' : minDate,
 			'maxDate' : maxDate,
 			'count': data.length
@@ -191,13 +184,40 @@ function drawchart_mv(dest_id, data, title) {
 	var height = svg.attr('height');
 
 	var stat = get_datastats_mv(data);
+	if (stat.min == 0 && stat.max == 0) {
+		stat.min = -.05;
+		stat.max = .05;
+	}
+	var pad = (stat.max - stat.min)*.1;
+	pad = stat.max != 0 ? Math.abs(pad/stat.max) : (stat.min != 0 ? Math.abs(pad/stat.min) : .05);
 	
 	var xScale = d3.scaleTime()
 		.domain([stat.minDate, stat.maxDate])
 		.range([chart_fmt.margin, width - chart_fmt.margin]);
 	var yScale = d3.scaleLinear()
-		.domain([stat.min == 0 ? -stat.max*.05 : stat.min*.95, stat.max*1.05])
+		.domain([stat.min == 0 ? -stat.max*pad : stat.min*(1-pad), stat.max*(1+pad)])
 		.range([height - chart_fmt.margin, chart_fmt.margin]);
+
+	// gridlines
+	function make_x_gridlines() {		
+		return d3.axisBottom(xScale).ticks();
+	}
+	function make_y_gridlines() {		
+		return d3.axisLeft(yScale).ticks();
+	}
+	svg.append("g")
+		.attr("class", "grid")
+		.style('shape-rendering', 'crispEdges')
+		.style('stroke-opacity', 0.2)
+		.style('stroke-dasharray', ('2, 2'))
+		.attr('transform', `translate(0,${height-chart_fmt.margin})`)
+		.call(make_x_gridlines().tickFormat('').tickSize(-height+2*chart_fmt.margin));
+	svg.append("g")			
+		.style('shape-rendering', 'crispEdges')
+		.style('stroke-opacity', 0.2)
+		.style('stroke-dasharray', ('2, 2'))
+		.attr("transform", `translate(${chart_fmt.margin},0)`)
+		.call(make_y_gridlines().tickFormat('').tickSize(-width+2*chart_fmt.margin));
 
 	// axises
 	svg.append("g")
@@ -210,8 +230,58 @@ function drawchart_mv(dest_id, data, title) {
 	svg.append("g")
 		.attr("class", "y axis")
 		.attr("transform", `translate(${chart_fmt.margin},0)`)
-		.call(d3.axisLeft(yScale));
-	
+		.call(d3.axisRight(yScale))
+	;
+
+	// stat data - must be behind main line
+	if (chart_fmt.is_statAsArea) {
+		var area = d3.area()
+			.curve(d3.curveMonotoneX)
+			.x(function(el) { return xScale(new Date(el.date + ' ' + el.time)); })
+			.y0(function(el) { return yScale(el.statvalue_lo); })
+			.y1(function(el) { return yScale(el.statvalue_hi); });
+
+		svg.append('path')
+			.datum(data)
+			.attr('d', area)
+			.style('stroke', chart_fmt.color_danger)
+			.style('stroke-width', 1)
+			.style('stroke-opacity', 0.4)
+			.style('fill', chart_fmt.color_danger)
+			.style('fill-opacity', 0.1)
+		;
+	}
+	else {
+		var statline_min = d3.line()
+			.x(function(el) { return xScale(new Date(el.date + ' ' + el.time)); })
+			.y(function(el) { 
+				return yScale(el.statvalue_lo);
+			})
+			.curve(d3.curveMonotoneX);	// smoothing line
+		var statline_max = d3.line()
+			.x(function(el) { return xScale(new Date(el.date + ' ' + el.time)); })
+			.y(function(el) { 
+				return yScale(el.statvalue_hi);
+			})
+			.curve(d3.curveMonotoneX);	// smoothing line
+		svg.append('path')
+			.datum(data)
+			.attr("d", statline_min)
+			.style('stroke', chart_fmt.color_danger)
+			.style('stroke-width', 1)
+			.style('stroke-dasharray', ('3, 3'))
+			.style('fill', 'none')
+		;
+		svg.append('path')
+			.datum(data)
+			.attr("d", statline_max)
+			.style('stroke', chart_fmt.color_danger)
+			.style('stroke-width', 1)
+			.style('stroke-dasharray', ('3, 3'))
+			.style('fill', 'none')
+		;
+	}
+
 	// data
 	var line = d3.line()
 		.x(function(el) { return xScale(new Date(el.date + ' ' + el.time)); })
@@ -225,7 +295,7 @@ function drawchart_mv(dest_id, data, title) {
 		.style('stroke-width', 1)
 		.style('fill', 'none')
 	;
-
+	
 	// zero line
 	if (chart_fmt.is_showZeroLine && (stat.min == 0 || (stat.min < 0 && stat.max > 0))) {
 		svg.append('line')
@@ -250,10 +320,22 @@ function drawchart_mv(dest_id, data, title) {
 			return;
 		svg.append('text')
 			.attr('x', 2*chart_fmt.margin)
-			.attr('y', chart_fmt.margin)
+			.attr('y', chart_fmt.margin-4)
 			.attr('class', 'ttip muted')
 			.html( chart_fmt.formatFullTime(xScale.invert(xy[0])) + ": " + yScale.invert(xy[1]).toFixed(chart_fmt.precision) );
 	});
+	
+	// legend
+	var legend = d3.select('#' + dest_id).append('div').attr('class', 'text-small text-info');
+	legend.html('<br /><h5>Legend:</h5>' +
+		'<ul>' + 
+			'<li><b>Solid Line</b> - exact metric values.</li>' +
+			'<li><b>Dashed Line</b> - historical min/max values for the same one hour period.</li>' + 
+		'</ul>' +
+		'<p>' +
+			'* - click on a chart to draw horizontal line for easy comparison of values.<br />' +
+		'</p>'
+	);
 
 	return true;
 }
@@ -263,17 +345,46 @@ function drawchart_mvdet(dest_id, data, title) {
 	var svg = drawchart_init(dest_id, title);
 	if (!svg)
 		return false;	// could not init chart.
+	
 	var width = svg.attr('width');
 	var height = svg.attr('height');
 
 	var stat = get_datastats_mvdet(data);
+	if (stat.min == 0 && stat.max == 0) {
+		stat.min = -.05;
+		stat.max = .05;
+	}
+	var pad = (stat.max - stat.min)*.1;
+	pad = stat.max != 0 ? Math.abs(pad/stat.max) : (stat.min != 0 ? Math.abs(pad/stat.min) : .05);
+	//alert(stat.min + ' - ' + stat.max + '; interval: ' + stat.timeInterval + '; pad:' + pad);
 	
 	var xScale = d3.scaleTime()
 		.domain([stat.minDate, new Date(stat.maxDate).setHours(stat.maxDate.getHours() + stat.timeInterval)])	// expand by timeInterval
 		.range([chart_fmt.margin, width - chart_fmt.margin]);
 	var yScale = d3.scaleLinear()
-		.domain([stat.min == 0 ? -stat.max*.05 : stat.min*.95, stat.max*1.05])
+		.domain([stat.min == 0 ? -stat.max*pad : stat.min*(1-pad), stat.max*(1+pad)])
 		.range([height - chart_fmt.margin, chart_fmt.margin]);
+
+	// gridlines
+	function make_x_gridlines() {		
+		return d3.axisBottom(xScale).ticks();
+	}
+	function make_y_gridlines() {		
+		return d3.axisLeft(yScale).ticks();
+	}
+	svg.append("g")
+		.attr("class", "grid")
+		.style('shape-rendering', 'crispEdges')
+		.style('stroke-opacity', 0.2)
+		.style('stroke-dasharray', ('2, 2'))
+		.attr('transform', `translate(0,${height-chart_fmt.margin})`)
+		.call(make_x_gridlines().tickFormat('').tickSize(-height+2*chart_fmt.margin));
+	svg.append("g")			
+		.style('shape-rendering', 'crispEdges')
+		.style('stroke-opacity', 0.2)
+		.style('stroke-dasharray', ('2, 2'))
+		.attr("transform", `translate(${chart_fmt.margin},0)`)
+		.call(make_y_gridlines().tickFormat('').tickSize(-width+2*chart_fmt.margin));
 
 	// axises
 	svg.append("g")
@@ -286,10 +397,69 @@ function drawchart_mvdet(dest_id, data, title) {
 	svg.append("g")
 		.attr("class", "y axis")
 		.attr("transform", `translate(${chart_fmt.margin},0)`)
-		.call(d3.axisLeft(yScale));
+		.call(d3.axisRight(yScale));
+
+	// stat data - must be behind main line
+	if (chart_fmt.is_statAsArea) {
+		var area = d3.area()
+			.curve(d3.curveMonotoneX)
+			.x(function(el) { 
+				return xScale(new Date(el.date + ' ' + el.time_start).setMinutes(new Date(el.date + ' ' + el.time_start).getMinutes() + stat.timeInterval*60/2));
+			})
+			.y0(function(el) { return yScale(el.statvalue_lo); })
+			.y1(function(el) { return yScale(el.statvalue_hi); });
+
+		svg.append('path')
+			.datum(data)
+			.attr('d', area)
+			.style('stroke', chart_fmt.color_danger)
+			.style('stroke-width', 1)
+			.style('stroke-opacity', 0.4)
+			.style('fill', chart_fmt.color_danger)
+			.style('fill-opacity', 0.1)
+		;
+	}
+	else {
+		var statline_min = d3.line()
+			.x(function(el) { 
+				return xScale(new Date(el.date + ' ' + el.time_start).setMinutes(new Date(el.date + ' ' + el.time_start).getMinutes() + stat.timeInterval*60/2));
+			})
+			.y(function(el) { 
+				//return yScale(el.statvalue_lo > el.statvalue_avg-2*el.statvalue_std ? el.statvalue_lo : el.statvalue_avg-2*el.statvalue_std); 
+				return yScale(el.statvalue_lo);
+			})
+			.curve(d3.curveMonotoneX);	// smoothing line
+		var statline_max = d3.line()
+			.x(function(el) { 
+				return xScale(new Date(el.date + ' ' + el.time_start).setMinutes(new Date(el.date + ' ' + el.time_start).getMinutes() + stat.timeInterval*60/2));
+				//return xScale(new Date(el.date + ' ' + el.time_start)); 
+			})
+			.y(function(el) { 
+				//return yScale(el.statvalue_hi < el.statvalue_avg+3*el.statvalue_std ? el.statvalue_hi : el.statvalue_avg+3*el.statvalue_std); 
+				return yScale(el.statvalue_hi);
+			})
+			.curve(d3.curveMonotoneX);	// smoothing line
+		svg.append('path')
+			.datum(data)
+			.attr("d", statline_min)
+			.style('stroke', chart_fmt.color_danger)
+			.style('stroke-width', 1)
+			.style('stroke-dasharray', ('3, 3'))
+			.style('fill', 'none')
+		;
+		svg.append('path')
+			.datum(data)
+			.attr("d", statline_max)
+			.style('stroke', chart_fmt.color_danger)
+			.style('stroke-width', 1)
+			.style('stroke-dasharray', ('3, 3'))
+			.style('fill', 'none')
+		;
+	}
 	
 	// data
-	var barWidth = Math.floor((width-chart_fmt.margin)/stat.count)-4;
+	//var barWidth = Math.floor((width-chart_fmt.margin)/stat.count)-4;
+	var barWidth = xScale(new Date(stat.minDate).setHours(stat.minDate.getHours() + stat.timeInterval)) - xScale(new Date(stat.minDate));
 	if (barWidth % 2 != 0) {
 		barWidth -= 1;
 	}
@@ -323,13 +493,13 @@ function drawchart_mvdet(dest_id, data, title) {
 		.attr('fill', function(el) { 
 			return el.statratio_descr != "OK" ? chart_fmt.color_danger : chart_fmt.color_primeline; }
 		)
-		.style('fill-opacity', 0.4)
+		.style('fill-opacity', 0.6)
 		.on('mouseover', function(el) {		
-			d3.select(this).style('fill-opacity', .8);
+			d3.select(this).style('fill-opacity', 1);
 			d3.select('text.ttip').remove();
 			svg.append('text')
 				.attr('x', 2*chart_fmt.margin)
-				.attr('y', chart_fmt.margin)
+				.attr('y', chart_fmt.margin-4)
 				.attr('class', 'ttip muted')
 				.html('Date: ' + chart_fmt.formatDate(new Date(el.date + ' 00:00:00')) + 
 					'; time: ' + chart_fmt.formatTime(new Date(el.date + ' ' + el.time_start)) + 
@@ -342,48 +512,11 @@ function drawchart_mvdet(dest_id, data, title) {
 			;
 		})					
 		.on('mouseout', function(el) {
-			d3.select(this).style('fill-opacity', .4);
+			d3.select(this).style('fill-opacity', .6);
 			d3.select('text.ttip').remove();
 		})
 	;
 	
-	// stat data - must be behind main line
-	var statline_min = d3.line()
-		.x(function(el) { 
-			return xScale(new Date(el.date + ' ' + el.time_start).setMinutes(new Date(el.date + ' ' + el.time_start).getMinutes() + stat.timeInterval*60/2));
-		})
-		.y(function(el) { 
-			//return yScale(el.statvalue_lo > el.statvalue_avg-2*el.statvalue_std ? el.statvalue_lo : el.statvalue_avg-2*el.statvalue_std); 
-			return yScale(el.statvalue_lo);
-		})
-		.curve(d3.curveMonotoneX);	// smoothing line
-	var statline_max = d3.line()
-		.x(function(el) { 
-			return xScale(new Date(el.date + ' ' + el.time_start).setMinutes(new Date(el.date + ' ' + el.time_start).getMinutes() + stat.timeInterval*60/2));
-			//return xScale(new Date(el.date + ' ' + el.time_start)); 
-		})
-		.y(function(el) { 
-			//return yScale(el.statvalue_hi < el.statvalue_avg+3*el.statvalue_std ? el.statvalue_hi : el.statvalue_avg+3*el.statvalue_std); 
-			return yScale(el.statvalue_hi);
-		})
-		.curve(d3.curveMonotoneX);	// smoothing line
-	svg.append('path')
-		.datum(data)
-		.attr("d", statline_min)
-		.style('stroke', chart_fmt.color_danger)
-		.style('stroke-width', 1)
-		.style("stroke-dasharray", ("3, 3"))
-		.style('fill', 'none')
-	;
-	svg.append('path')
-		.datum(data)
-		.attr("d", statline_max)
-		.style('stroke', chart_fmt.color_danger)
-		.style('stroke-width', 1)
-		.style("stroke-dasharray", ("3, 3"))
-		.style('fill', 'none')
-	;
-
 	// zero line
 	if (chart_fmt.is_showZeroLine && (stat.min == 0 || (stat.min < 0 && stat.max > 0))) {
 		svg.append('line')
@@ -398,5 +531,19 @@ function drawchart_mvdet(dest_id, data, title) {
 			;
 	};
 
+	// legend
+	var legend = d3.select('#' + dest_id).append('div').attr('class', 'text-small text-info');
+	legend.html('<br /><h5>Legend:</h5>' +
+		'<ul>' + 
+			'<li><b>Box</b> - represents aggregation time period with min and max values as its lower and higher bars.</li>' +
+			'<li><b>Solid Line</b> - average values for the period.</li>' +
+			'<li><b>Dashed Line</b> - historical min/max values for the same period.</li>' + 
+		'</ul>' +
+		'<p>' +
+			'* - click on a chart to draw horizontal line for easy comparison of values.<br />' +
+			'* - hover over box to show period details.' +
+		'</p>'
+	);
+	
 	return true;
 }
